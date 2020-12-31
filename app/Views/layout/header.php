b286893fadad4f2a4c1d702fccf6e1430bf4639d
Sayid Ghufron <sayid.ghufron123@gmail.com>
2021-01-01 06:25:42 +0700

    bugfix

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -45,7 +45,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><img src=<?= base_url('logo.png') ?> alt="" style="width: 15%;"></a>
+                    <a class="navbar-brand" href="<?php echo base_url('Home/index') ?>"><img src=<?= base_url('logo.png') ?> alt="" style="width: 15%;"></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
